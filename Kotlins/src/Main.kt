fa0b2b2ff263ad30b50a132b739288f11d82a5e4
Jaguszeskie77 <148161981+Jaguszeskie77@users.noreply.github.com>
2024-08-14 22:03:25 -0300

    Atividade 01 - Manipulação de variáveis Kotlin

---
+++ Kotlins/src/Main.kt
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    val nome: String = "Emanoel"
+    val idade: Int = 19
+    val altura: Double = 1.70
+    val eEstudante: Boolean = true
+
+    var somaidade : Int = idade + idade
+    var multaltura : Double = altura * 2
+
+    var saudacao : String ="Olá,$nome "
+
+    println("Ola $nome, seu lindão")
+    println( "Idade:$idade")
+    println("Altura:$altura")
+    println("É estudante:$eEstudante")
+    println("Soma das idades:$somaidade" )
+    println("Altura dobrada:$multaltura" )
+    println(saudacao)
+
+
+
+
+}
